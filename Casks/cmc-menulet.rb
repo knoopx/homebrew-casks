c275c02353220b2bffe3e0c21e95fd37764aadc5
Víctor Martínez <knoopx@gmail.com>
2017-10-19 10:42:01 +0200

    Create cmc-menulet.rb

---
+++ Casks/cmc-menulet.rb
@@ -0,0 +1,11 @@
+
+cask 'cmc-menulet' do
+  version '0.1.0'
+  sha256 ''
+
+  url 'https://github.com/knoopx/cmc-menulet/releases/download/0.1.0/CoinMarketCap.zip'
+  name 'CoinMarketCap'
+  homepage 'https://github.com/knoopx/cmc-menulet'
+
+  app 'CoinMarketCap.app'
+end
