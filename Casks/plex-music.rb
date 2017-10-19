e976b4a5a0727e0fcc0f4a57f88dbf6c648e4dd1
Víctor Martínez <knoopx@gmail.com>
2017-10-19 14:53:34 +0200

    Update plex-music.rb

--- Casks/plex-music.rb
+++ Casks/plex-music.rb
@@ -1,6 +1,6 @@
 cask 'plex-music' do
   version 'v1.0.4'
-  sha256 '54e5ff8614777f7a47014c30e8d43b9b0cf6df49'
+  sha256 '011976d3ffd4e269b80579f97927c71a3cfd0871ffa045f5913a55d2fb7a6e8b'
 
   url 'https://github.com/knoopx/plex-music/releases/download/v1.0.4/Plex.Music.zip'
   name 'Plex Music'
