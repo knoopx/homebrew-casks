067d265a803f1cc8117621a63bd785b4c5c5df60
Víctor Martínez <knoopx@gmail.com>
2017-10-19 10:47:52 +0200

    Update cmc-menulet.rb

--- Casks/cmc-menulet.rb
+++ Casks/cmc-menulet.rb
@@ -1,7 +1,7 @@
 
 cask 'cmc-menulet' do
   version '0.1.0'
-  sha256 ''
+  sha256 'e1d74acd2c77a911a1ae92c7037fc5aa50eee5857ba7c2851ab1de1211468f97'
 
   url 'https://github.com/knoopx/cmc-menulet/releases/download/0.1.0/CoinMarketCap.zip'
   name 'CoinMarketCap'
