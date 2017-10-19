523a3202ceab8b5e685a6921f6b8c0c82610806e
Víctor Martínez <knoopx@gmail.com>
2017-10-19 10:38:14 +0200

    Create plex-music.rb

---
+++ Casks/plex-music.rb
@@ -0,0 +1,10 @@
+cask 'plex-music' do
+  version '1.0.3'
+  sha256 'c11e1f1fd8add8eeae4d5d15f70526d476f11569af72f4553760ee196df2f112'
+
+  url 'https://github.com/knoopx/plex-music/releases/download/v1.0.3/Plex.Music.zip'
+  name 'Plex Music'
+  homepage 'https://github.com/knoopx/plex-music/'
+
+  app 'Plex Music.app'
+end
